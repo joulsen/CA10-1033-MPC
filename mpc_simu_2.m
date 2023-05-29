7d3ee838fbe6056a9ddadbf3b111d0c67cdf943e
Andreas J. P <supercigar@gmail.com>
2023-05-29 23:19:16 +0200

    Cleanup, tuning and fix kappa matching

--- mpc_simu_2.m
+++ mpc_simu_2.m
@@ -3,17 +3,18 @@ clear mpc_simulation
 
 data = read_bitzer_data("../../BitzerData/Logdata-d-60/HPLog 026.csv", [1, -1]);
 steps = size(data, 1);
-d = data.Tamb;
+% steps = 540;
+d = data.Tamb(1:steps);
 
 % Defining parameters for the MPC
-param = struct("Hu", 20, "Hp", 1000, ...
+param = struct("Hu", 20, "Hp", 25, ...
                "mu1", 1, "mu2", 60, "mu3", 2, "mu4", 0.1, ...
                "x0", [35; 30; 25; -2; 20], ...
                "P", [0.01, 0.02, 0.03, 0.04, 0.05], ...
                "kappa", 30);
-param.u0 = d(1) + param.mu3;
+param.u0 = 30;
 param.Q = eye(param.Hp);
-param.R = 10*eye(param.Hu);
+param.R = 0.1*eye(param.Hu);
 param = mpc_iteration_initialize(param);
 sys = param.sys;
 
@@ -23,8 +24,18 @@ options = struct("MPC_Enabled", true, "Input_Default", 0);
 % u = zeros(steps, 1);
 r = ones(steps, 1)*25;
 tic
-[u, x, xest, y] = mpc_simulation(r, d, param, options);
+[u, x, xest, y, z] = mpc_simulation(r, d, param, options);
 toc
 
 clf
+subplot(3,1,1)
+hold on
+stairs(z)
+stairs(r)
+subplot(3,1,2)
+hold on
+stairs(u)
+stairs(d)
+subplot(3,1,3)
 stairs(x)
+
--- mpc_simulation.m
+++ mpc_simulation.m
@@ -1,4 +1,4 @@
-function [u, x, xest, y_C] = mpc_simulation(r, d, param, options)
+function [u, x, xest, y_C, z] = mpc_simulation(r, d, param, options)
     persistent sys x0 u0 us Hu Hp;
     steps = size(r, 1);
     if isempty(x0)
@@ -10,11 +10,14 @@ function [u, x, xest, y_C] = mpc_simulation(r, d, param, options)
     end
     u = zeros(steps, 1);
     x = zeros(steps, size(sys.A, 1));
+    x(1,:) = param.x0;
     xest = zeros(steps, size(sys.A, 1));
     y_C = zeros(steps, size(sys.Cy, 1));
+    z = zeros(steps, size(sys.Cz, 1));
     tstart = 2;
     t = 2:steps;
     for k=t
+        % Move inputs and output forward
         ri = r(k:end,:);
         di = d(k:end,:);
         y = y_C(k-1,:);
@@ -24,13 +27,20 @@ function [u, x, xest, y_C] = mpc_simulation(r, d, param, options)
         xest(k,:) = x0;
         % Controller
         if (~mod(k - tstart, param.kappa))
-            try
-                deltaU = mpc_iteration_base(x0, ri, di, param, options);
-            catch ME
-                disp("error");
+            [dbar, Hp_bar] = match_Hp(di, Hp, param.kappa);
+            Hu_bar = min(param.Hu, Hp_bar);
+            rbar = ri(1:param.kappa:param.kappa*Hp_bar);
+            if Hp_bar == 0
+                return
             end
-            us = u0 + deltaU;
-            us = interp1(k + (0:Hu-1)*param.kappa, us, 1:steps)';
+            deltaU = mpc_iteration_base(x0, rbar, dbar, param, options);
+            if size(deltaU, 1) > 1
+                us = u0 + deltaU;
+                us = interp1(k + (0:Hu_bar-1)*param.kappa, us, 1:steps, "previous")';
+            else
+                us = zeros(size(us));
+                us(k:end) = u0 + deltaU;
+            end
         end
 %         deltaU = mpc_iteration_base(x0, ri, di, param, options);
 %         u0 = u0 + deltaU(1);
@@ -39,31 +49,50 @@ function [u, x, xest, y_C] = mpc_simulation(r, d, param, options)
         % Model
         x(k,:) = sys.A * x(k-1,:)' + sys.B * u(k-1,:)' + sys.Bd * d(k-1,:)';
         y_C(k,:) = sys.Cy * x(k,:)' + sys.Dy * u(k,:)' + sys.Ddy * d(k-1,:)';
+        z(k,:) = sys.Cz * x(k,:)' + sys.Dz * u(k,:)' + sys.Ddz * d(k-1,:)';
+    end
+    function [mat, Hp_bar] = match_Hp(mat, Hp, kappa)
+        v = size(mat, 1);
+        Hp_bar = min(Hp, floor(v / kappa));
+        mat = mat(1:kappa:Hp_bar*kappa);
     end
     function deltaU = mpc_iteration_base(x, r, d, param, options)
         %MPC_ITERATION_BASE Performs a single iteration of the MPC
         if options.MPC_Enabled
-            r = match_Hp(r, Hp);
-            d = match_Hp(d, Hp);
-            Eps = r - param.Psi * x - param.Ups * u0 - param.Xi * d;
-            G = 2*param.Theta' * param.Q * Eps;
-            [A_con, b_con] = get_lifted_constraints(Hu, u0, d);
+%             [d, Hp_bar] = match_Hp(d, Hp, param.kappa);
+%             Hu_bar = min(param.Hu, Hp_bar);
+%             r = r(1:param.kappa:param.kappa*Hp_bar);
+            % Cropping to Hp_bar
+            dim = struct("p", size(param.msys.Cz, 1), ...
+                         "m", size(param.msys.A, 1), ...
+                         "n", size(param.msys.B, 2), ...
+                         "j", size(param.msys.Bd, 2));
+            Psi = param.Psi(1:dim.p*Hp_bar, :);
+            Ups = param.Ups(1:dim.p*Hp_bar, :);
+            Theta = param.Theta(1:dim.p*Hp_bar, 1:dim.n*Hu_bar);
+            Xi = param.Xi(1:dim.p*Hp_bar, 1:dim.j*Hp_bar);
+            Q = param.Q(1:Hp_bar, 1:Hp_bar);
+            R = param.R(1:Hu_bar, 1:Hu_bar);
+            H = Theta' * Q * Theta + R;
+            H = (H + H')/2;
+%             Eps = r - param.Psi * x - param.Ups * u0 - param.Xi * d;
+%             G = 2*param.Theta' * param.Q * Eps;
+            Eps = r - Psi * x - Ups * u0 - Xi * d;
+            G = 2*Theta' * Q * Eps;
+            [A_con, b_con] = get_lifted_constraints(Hu_bar, u0, d);
             qp_opt = optimoptions('quadprog', 'Algorithm', 'active-set', ...
                                   'Display', 'off');
-            deltaU = quadprog(2*param.H, -G', A_con, b_con, ...
-                              [],[],[],[],zeros(param.Hu, 1),qp_opt);
+            deltaU = quadprog(2*H, -G', A_con, b_con, ...
+                              [],[],[],[],zeros(Hu_bar, 1),qp_opt);
 %             u = u0 + deltaU(1);
         else
 %             u = options.Input_Default;
             deltaU = 0;
         end
-        function mat = match_Hp(mat, Hp)
-            size_diff = size(mat, 1) - Hp;
-            if size_diff < 0
-                mat = [mat; kron(ones(abs(size_diff), 1), mat(end,:))];
-            elseif size_diff > 0
-                mat = mat(1:Hp);
-            end
+        function [mat, Hp_bar] = match_Hp(mat, Hp, kappa)
+            v = size(mat, 1);
+            Hp_bar = min(Hp, floor(v / kappa));
+            mat = mat(1:kappa:Hp_bar*kappa);
         end
         function [A_con, b_con] = get_lifted_constraints(Hu, u0, d)
             nu = 1;
@@ -71,7 +100,7 @@ function [u, x, xest, y_C] = mpc_simulation(r, d, param, options)
             % Input constraint F
             F = kron(eye(Hu, Hu), [-1; 1; -1]);
             f1 = kron(ones(Hu, 1), [param.mu1; -param.mu2; param.mu3]);
-            f2 = kron(eye(Hu), [0; 0; 1]) * d(1:param.kappa:Hu*param.kappa);
+            f2 = kron(eye(Hu), [0; 0; 1]) * d(1:Hu);
             f = f1+f2;
             F = [F, f];
             % Input rate constrain E = [W w]
